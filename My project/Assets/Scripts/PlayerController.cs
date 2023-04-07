44b0669449523d7227ac6ba2e8fb972b9cfb9670
Gözde <ozlemgozdeozkan@gmail.com>
2023-04-07 22:04:03 +0300

    Gözde 2

--- My project/Assets/Scripts/PlayerController.cs
+++ My project/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,13 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < 100; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Debug.Log(i);
+            }
+        }
         Debug.Log("Ömer baþardýn");
     }
 
