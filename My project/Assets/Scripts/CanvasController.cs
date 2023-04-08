9be3f9c7a354b5cee8e9c9f2917869f9d1703876
Gözde <ozlemgozdeozkan@gmail.com>
2023-04-09 01:14:18 +0300

    gözde 1500

---
+++ My project/Assets/Scripts/CanvasController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CanvasController : MonoBehaviour
+{
+
+    public GameObject introPanel, playGamePanel, rawImagePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        introPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
